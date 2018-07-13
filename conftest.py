a590f69ac0a2a6d16555d93d9eab690675bba124
Sean Coates <sean@seancoates.com>
2018-07-13 14:06:45 -0400

    black.

--- conftest.py
+++ conftest.py
@@ -6,7 +6,14 @@ import sys
 import socket
 import hashlib
 from jinja2 import Template
-from zappa_e2e import PreservableTemporaryDirectory, DeployedZappaApp, venv_cmd, chdir, ENV_CONFIG, python_executables
+from zappa_e2e import (
+    PreservableTemporaryDirectory,
+    DeployedZappaApp,
+    venv_cmd,
+    chdir,
+    ENV_CONFIG,
+    python_executables,
+)
 import subprocess
 import time
 
@@ -16,20 +23,24 @@ DIR = os.path.realpath(os.path.dirname(__file__))
 APPS_PREFIX = os.path.join(DIR, "apps")
 logger = logging.getLogger()
 
-ZAPPA_S3_BUCKET = os.environ.get('ZAPPA_S3_BUCKET', "zappa-e2e-" + str(hashlib.md5(socket.gethostname().encode()).hexdigest()))
+ZAPPA_S3_BUCKET = os.environ.get(
+    "ZAPPA_S3_BUCKET",
+    "zappa-e2e-" + str(hashlib.md5(socket.gethostname().encode()).hexdigest()),
+)
 logger.debug("Zappa E2E: using s3 bucket " + ZAPPA_S3_BUCKET)
+
 
 def _path_to_app(path):
     str_path = str(path)
     if str_path.startswith(APPS_PREFIX):
-        apps_path = str_path[len(APPS_PREFIX)+1:]
-        return apps_path.split('/', 1)
+        apps_path = str_path[len(APPS_PREFIX) + 1 :]
+        return apps_path.split("/", 1)
     return None, None
 
 
 def pytest_collect_file(parent, path):
     app_name, sub_path = _path_to_app(path)
-    if sub_path == 'zappa_settings.json.j2':
+    if sub_path == "zappa_settings.json.j2":
         return ZappaAppFile(path, parent)
 
 
@@ -53,10 +64,8 @@ class ZappaAppTest(pytest.Item):
         self.app_name = parent.app_name
         self.app_path = parent.app_path
 
-
     def _venv_cmd(self, cmd, params=[], as_json=False, check=False, extra_env={}):
         return venv_cmd(self.venv_dir, cmd, params, as_json, check, extra_env)
-
 
     def runtest(self):
 
@@ -65,76 +74,107 @@ class ZappaAppTest(pytest.Item):
             if self.__class__.first_run:
                 self.__class__.first_run = False
             else:
-                logger.info("Sleeping for {}s to help with the AWS rate limit.".format(SLEEP_BETWEEN))
+                logger.info(
+                    "Sleeping for {}s to help with the AWS rate limit.".format(
+                        SLEEP_BETWEEN
+                    )
+                )
                 time.sleep(SLEEP_BETWEEN)
 
             if py_executable is None:
                 logger.warn("Could not find a python {} executable.".format(py_version))
                 continue
 
-            logger.info("Entering app {} with Python {}".format(self.app_name, py_version))
-            with PreservableTemporaryDirectory(self.app_name, 'py' + py_version) as (app_tmp_dir, ptd):
-                self.app_test_dir = os.path.join(app_tmp_dir, "{}-py{}".format(self.app_name, py_version))
+            logger.info(
+                "Entering app {} with Python {}".format(self.app_name, py_version)
+            )
+            with PreservableTemporaryDirectory(self.app_name, "py" + py_version) as (
+                app_tmp_dir,
+                ptd,
+            ):
+                self.app_test_dir = os.path.join(
+                    app_tmp_dir, "{}-py{}".format(self.app_name, py_version)
+                )
                 copy_tree(self.app_path, self.app_test_dir)
                 chdir(self.app_test_dir)
 
-                self.venv_dir = os.path.join(app_tmp_dir, 'venv')
+                self.venv_dir = os.path.join(app_tmp_dir, "venv")
 
-                req_path = os.path.join(self.app_test_dir, 'requirements.txt')
+                req_path = os.path.join(self.app_test_dir, "requirements.txt")
                 if os.path.isfile(req_path):
                     requirements_txt_path = req_path
 
-                alt_req_path = os.path.join(self.app_test_dir, 'requirements-py{}.txt'.format(py_version.replace('.', '')))
+                alt_req_path = os.path.join(
+                    self.app_test_dir,
+                    "requirements-py{}.txt".format(py_version.replace(".", "")),
+                )
                 if os.path.isfile(alt_req_path):
                     # override:
                     requirements_txt_path = alt_req_path
 
                 if not os.path.isdir(self.venv_dir):
-                    cmd = subprocess.run(['virtualenv', '-p', py_executable, self.venv_dir], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                    cmd = subprocess.run(
+                        ["virtualenv", "-p", py_executable, self.venv_dir],
+                        stdout=subprocess.PIPE,
+                        stderr=subprocess.PIPE,
+                    )
                     if cmd.returncode != 0:
                         print(cmd.returncode, cmd.stdout, cmd.stderr)
-                        raise EnvironmentError("Could not create virtualenv for py {}".format(py_version))
+                        raise EnvironmentError(
+                            "Could not create virtualenv for py {}".format(py_version)
+                        )
 
-                ret, _, _ = self._venv_cmd('pip', ['install', '-r', 'requirements.txt'], check=True)
+                ret, _, _ = self._venv_cmd(
+                    "pip", ["install", "-r", "requirements.txt"], check=True
+                )
 
-                template_file = os.path.join(self.app_test_dir, "zappa_settings.json.j2")
+                template_file = os.path.join(
+                    self.app_test_dir, "zappa_settings.json.j2"
+                )
                 with open(template_file) as f:
                     template_source = f.read()
 
                 template = Template(template_source)
                 rendered_template = template.render(
-                    S3_BUCKET=ZAPPA_S3_BUCKET,
-                    E2E_VERSION=py_version
+                    S3_BUCKET=ZAPPA_S3_BUCKET, E2E_VERSION=py_version
                 )
 
                 settings_file = os.path.join(self.app_test_dir, "zappa_settings.json")
-                with open(settings_file, 'w') as zsj:
+                with open(settings_file, "w") as zsj:
                     zsj.write(rendered_template)
-                    logger.debug("Zappa E2E: wrote settings file {} from template {}".format(
-                        settings_file, template_file
-                    ))
+                    logger.debug(
+                        "Zappa E2E: wrote settings file {} from template {}".format(
+                            settings_file, template_file
+                        )
+                    )
 
-                with DeployedZappaApp(self.app_test_dir, self.venv_dir, ptd) as zappa_app:
+                with DeployedZappaApp(
+                    self.app_test_dir, self.venv_dir, ptd
+                ) as zappa_app:
 
                     # undeployed handled in DeployedZappaApp
-                    if not ENV_CONFIG['undeploy_only']:
+                    if not ENV_CONFIG["undeploy_only"]:
 
-                        ret, status, _ = self._venv_cmd('zappa', ['status'], as_json=True)
+                        ret, status, _ = self._venv_cmd(
+                            "zappa", ["status"], as_json=True
+                        )
                         assert ret == 0, "Got Zappa app status"
 
-                        env_status = {
-                            'PY_VERSION': py_version,
-                        }
+                        env_status = {"PY_VERSION": py_version}
                         for k, v in status.items():
                             if type(v) == str:
-                                env_status[k.upper().replace(' ', '_')] = v
+                                env_status[k.upper().replace(" ", "_")] = v
 
-                        run_tests = os.path.join(self.app_test_dir, 'run_tests')
+                        run_tests = os.path.join(self.app_test_dir, "run_tests")
                         if os.path.isfile(run_tests):
-                            ret, out, err = self._venv_cmd(run_tests, extra_env=env_status)
+                            ret, out, err = self._venv_cmd(
+                                run_tests, extra_env=env_status
+                            )
                             if ret != 0:
                                 logger.info("stdout:")
                                 logger.info(out)
                                 logger.info("stderr:")
                                 logger.info(err)
-                            assert ret == 0 or ret == 5, "run_tests success (or no tests)"
+                            assert (
+                                ret == 0 or ret == 5
+                            ), "run_tests success (or no tests)"
--- zappa_e2e.py
+++ zappa_e2e.py
@@ -14,33 +14,34 @@ logger = logging.getLogger()
 
 
 def env_bool(var):
-    return os.environ.get("ZAPPA_E2E_" + var, False) in [1, '1', 'True', 'true', 'TRUE', True]
+    return os.environ.get("ZAPPA_E2E_" + var, False) in [
+        1,
+        "1",
+        "True",
+        "true",
+        "TRUE",
+        True,
+    ]
 
 
 ENV_CONFIG = {
     # harnesses the test suite to only undeploy apps, if possible. Does not test. Main use here is to clean up after a catastrophic mess, if even possible.
-    'undeploy_only': env_bool("UNDEPLOY_ONLY"),
-
+    "undeploy_only": env_bool("UNDEPLOY_ONLY"),
     # if an app is deployed, update instead of deploy
-    'update_over_deploy': env_bool("UPDATE_OVER_DEPLOY"),
-
+    "update_over_deploy": env_bool("UPDATE_OVER_DEPLOY"),
     # do not undeploy apps
-    'no_undeploy': env_bool("NO_UNDEPLOY"),
-
+    "no_undeploy": env_bool("NO_UNDEPLOY"),
     # preserve temporary app dirs
-    'preserve_temp': env_bool("PRESERVE_TEMP"),
-
+    "preserve_temp": env_bool("PRESERVE_TEMP"),
     # python versions
-    'skip_python_27': env_bool('SKIP_PYTHON_27'),
-    'skip_python_36': env_bool('SKIP_PYTHON_36'),
-
-    'python_27_path': env_bool('PYTHON_27_PATH'),
-    'python_36_path': env_bool('PYTHON_36_PATH'),
+    "skip_python_27": env_bool("SKIP_PYTHON_27"),
+    "skip_python_36": env_bool("SKIP_PYTHON_36"),
+    "python_27_path": env_bool("PYTHON_27_PATH"),
+    "python_36_path": env_bool("PYTHON_36_PATH"),
 }
 
 
 class PreservableTemporaryDirectory(tempfile.TemporaryDirectory):
-
     def __init__(self, app_name, version):
         """Preservable Temporary Directory
 
@@ -68,33 +69,34 @@ class PreservableTemporaryDirectory(tempfile.TemporaryDirectory):
 
         self.name = dir
         self._finalizer = weakref.finalize(
-            self, self._cleanup, self.name,
-            warn_message="Implicitly cleaning up {!r}".format(self))
+            self,
+            self._cleanup,
+            self.name,
+            warn_message="Implicitly cleaning up {!r}".format(self),
+        )
 
         self._preserve = True
 
-        if ENV_CONFIG['preserve_temp']:
+        if ENV_CONFIG["preserve_temp"]:
             logger.info("Automatically preserving temp dir due to environment config")
             self.preserve()
 
-
     def preserve(self):
         self._finalizer.detach()
-
 
     @property
     def preserved(self):
         return self._preserve
 
-
     def __enter__(self):
         return self.name, self
 
-
     def __exit__(self, exc, value, tb):
-        logger.debug("Exiting PreservableTemporaryDirectory {} with preserve={}".format(
-            self.name, "True" if self._preserve else "False"
-        ))
+        logger.debug(
+            "Exiting PreservableTemporaryDirectory {} with preserve={}".format(
+                self.name, "True" if self._preserve else "False"
+            )
+        )
         if not self._preserve:
             self.cleanup()
 
@@ -108,7 +110,6 @@ class DeployedZappaApp:
         self.failed = False
         self.stage = stage
         self.post_deploy_status = {}
-
 
     @property
     def status(self):
@@ -144,25 +145,28 @@ class DeployedZappaApp:
         """
         return self.post_deploy_status
 
-
     @property
     def name(self):
-        return self.post_deploy_status.get('Lambda Name', '(no name)')
-
+        return self.post_deploy_status.get("Lambda Name", "(no name)")
 
     def __repr__(self):
         return "<{} {!r}>".format(self.__class__.__name__, self.name)
 
-
     def __enter__(self):
         chdir(self.app_dir)
-        ret, out, _ = venv_cmd(self.venv_dir, 'zappa', ['status', '--json'])  # not using as_json because zappa status doesn't return json when it fails
-        pre_deploy_status_exists = (ret == 1)
+        ret, out, _ = venv_cmd(
+            self.venv_dir, "zappa", ["status", "--json"]
+        )  # not using as_json because zappa status doesn't return json when it fails
+        pre_deploy_status_exists = ret == 1
 
-        if not pre_deploy_status_exists and not (ENV_CONFIG['update_over_deploy'] or ENV_CONFIG['undeploy_only']):
-            logger.error("{}: Status succeeded before deploy. This probably means that the app is already deployed. Bailing.".format(
-                self.__class__.__name__
-            ))
+        if not pre_deploy_status_exists and not (
+            ENV_CONFIG["update_over_deploy"] or ENV_CONFIG["undeploy_only"]
+        ):
+            logger.error(
+                "{}: Status succeeded before deploy. This probably means that the app is already deployed. Bailing.".format(
+                    self.__class__.__name__
+                )
+            )
             self.skip_cleanup = True
             sys.exit(1)
 
@@ -172,116 +176,139 @@ class DeployedZappaApp:
             except json.decoder.JSONDecodeError:
                 pass
 
-        if ENV_CONFIG['undeploy_only']:
+        if ENV_CONFIG["undeploy_only"]:
             # exit early
             logger.info("{}: doing undeploy only".format(self.__class__.__name__))
             return self.name
 
-        if not pre_deploy_status_exists and ENV_CONFIG['update_over_deploy']:
-            logger.info("{}: updating instead of deploying for {}".format(
-                self.__class__.__name__, self.name
-            ))
-            ret, out, err = venv_cmd(self.venv_dir, 'zappa', ['update', self.stage])
+        if not pre_deploy_status_exists and ENV_CONFIG["update_over_deploy"]:
+            logger.info(
+                "{}: updating instead of deploying for {}".format(
+                    self.__class__.__name__, self.name
+                )
+            )
+            ret, out, err = venv_cmd(self.venv_dir, "zappa", ["update", self.stage])
             if ret != 0:
-                logger.error("{}: failed to update with message '{}'. Bailing. stderr={}".format(
-                    self.__class__.__name__, out, err
-                ))
+                logger.error(
+                    "{}: failed to update with message '{}'. Bailing. stderr={}".format(
+                        self.__class__.__name__, out, err
+                    )
+                )
                 return None
 
         else:
-            ret, out, err = venv_cmd(self.venv_dir, 'zappa', ['deploy', self.stage])
+            ret, out, err = venv_cmd(self.venv_dir, "zappa", ["deploy", self.stage])
             if ret != 0:
-                logger.error("{}: failed to deploy with message '{}'. Bailing. stderr={}".format(
-                    self.__class__.__name__, out, err
-                ))
+                logger.error(
+                    "{}: failed to deploy with message '{}'. Bailing. stderr={}".format(
+                        self.__class__.__name__, out, err
+                    )
+                )
                 self.skip_cleanup = True
                 return None
 
-        ret, out, _ = venv_cmd(self.venv_dir, 'zappa', ['status', self.stage], as_json=True)
+        ret, out, _ = venv_cmd(
+            self.venv_dir, "zappa", ["status", self.stage], as_json=True
+        )
         if ret != 0:
-            logger.error("{}: something went wrong with the post-deploy status check.".format(
-                self.__class__.__name__
-            ))
+            logger.error(
+                "{}: something went wrong with the post-deploy status check.".format(
+                    self.__class__.__name__
+                )
+            )
             self.failed = True
             return None
 
         self.post_deploy_status = out
-        logger.info("{}: zappa app {} published.".format(
-            self.__class__.__name__, self.name
-        ))
+        logger.info(
+            "{}: zappa app {} published.".format(self.__class__.__name__, self.name)
+        )
 
         return self.post_deploy_status
-
 
     def __exit__(self, exc, value, tb):
         self.cleanup()
 
-
     def _preserve_and_fail(self, msg):
         self.failed = True
         self.ptd.preserve()
-        logger.error("{}: failing with message '{}'. App directory preserved at: {}".format(
-            self.__class__.__name__, msg, self.ptd.name
-        ))
-
+        logger.error(
+            "{}: failing with message '{}'. App directory preserved at: {}".format(
+                self.__class__.__name__, msg, self.ptd.name
+            )
+        )
 
     def cleanup(self):
         if self.skip_cleanup:
-            logger.warn("{}: Skipping cleanup for {} in {}".format(
-                self.__class__.__name__, self.name, self.app_dir
-            ))
+            logger.warn(
+                "{}: Skipping cleanup for {} in {}".format(
+                    self.__class__.__name__, self.name, self.app_dir
+                )
+            )
             if self.failed:
                 self._preserve_and_fail("Failed before cleanup.")
         else:
-            logger.debug("{}: Cleaning up for {} in {}".format(
-                self.__class__.__name__, self.name, self.app_dir
-            ))
+            logger.debug(
+                "{}: Cleaning up for {} in {}".format(
+                    self.__class__.__name__, self.name, self.app_dir
+                )
+            )
             if self.failed:
                 self._preserve_and_fail("Failed before cleanup.")
             else:
-                if ENV_CONFIG['no_undeploy']:
-                    logger.info("Not undeploying {} due to environment config".format(self.name))
+                if ENV_CONFIG["no_undeploy"]:
+                    logger.info(
+                        "Not undeploying {} due to environment config".format(self.name)
+                    )
                 else:
                     os.chdir(self.app_dir)
-                    ret, out, err = venv_cmd(self.venv_dir, 'zappa', ['undeploy', '-y', self.stage])
-                    if ret == 0 or ENV_CONFIG['undeploy_only']:
-                        ret, out, _ = venv_cmd(self.venv_dir, 'zappa', ['status'])
+                    ret, out, err = venv_cmd(
+                        self.venv_dir, "zappa", ["undeploy", "-y", self.stage]
+                    )
+                    if ret == 0 or ENV_CONFIG["undeploy_only"]:
+                        ret, out, _ = venv_cmd(self.venv_dir, "zappa", ["status"])
                         if ret != 1:
-                            self._preserve_and_fail("Zappa status should have returned 1. Returned {}. With output: {}".format(
-                                ret, out
-                            ))
+                            self._preserve_and_fail(
+                                "Zappa status should have returned 1. Returned {}. With output: {}".format(
+                                    ret, out
+                                )
+                            )
 
                     else:
                         self._preserve_and_fail("Could not undeploy")
-                        logger.error("{}: Zappa failed to undeploy.\nstderr={}\nstdout={}".format(
-                            self.__class__.__name__, err, out
-                        ))
+                        logger.error(
+                            "{}: Zappa failed to undeploy.\nstderr={}\nstdout={}".format(
+                                self.__class__.__name__, err, out
+                            )
+                        )
 
             if self.failed:
                 sys.exit(1)
 
 
 def venv_cmd(venv_dir, cmd, params=[], as_json=False, check=False, extra_env={}):
-    args = [os.path.join(venv_dir, 'bin', cmd)]
+    args = [os.path.join(venv_dir, "bin", cmd)]
     args.extend(params)
     if as_json:
-        args.append('--json')
+        args.append("--json")
     logger.debug("Calling '{}'".format(" ".join(args)))
 
     env = copy(os.environ)
     env.update(extra_env)
 
     prefix = 'VIRTUAL_ENV="'
-    with open(os.path.join(venv_dir, 'bin', 'activate')) as f:
+    with open(os.path.join(venv_dir, "bin", "activate")) as f:
         l = f.readline()
-        while (l):
+        while l:
             if l.startswith(prefix):
-                env['VIRTUAL_ENV'] = l[len(prefix):-2]
+                env["VIRTUAL_ENV"] = l[len(prefix) : -2]
                 break
             l = f.readline()
 
-    #logger.debug("venv_cmd: calling {} with env {}".format(args, env))
-    cmd = subprocess.run(args, check=check, stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=env)
+    # logger.debug("venv_cmd: calling {} with env {}".format(args, env))
+    cmd = subprocess.run(
+        args, check=check, stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=env
+    )
     if as_json:
         try:
             return cmd.returncode, json.loads(cmd.stdout), cmd.stderr
@@ -301,7 +328,9 @@ def _try_run_python(name):
     cmd = find_executable(name)
     if cmd:
         try:
-            run = subprocess.run([cmd, '-V'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+            run = subprocess.run(
+                [cmd, "-V"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT
+            )
             if run:
                 out = run.stdout.decode()
         except DistutilsExecError:
@@ -311,41 +340,42 @@ def _try_run_python(name):
 
 def python_executables():
 
-    found = {
-        '2.7': None,
-        '3.6': None,
-    }
+    found = {"2.7": None, "3.6": None}
 
-    if ENV_CONFIG['python_27_path']:
-        out, cmd = _try_run_python(ENV_CONFIG['python_27_path'])
-        if out.startswith('Python 2.7'):
-            found['2.7'] = ENV_CONFIG['python_27_path']
+    if ENV_CONFIG["python_27_path"]:
+        out, cmd = _try_run_python(ENV_CONFIG["python_27_path"])
+        if out.startswith("Python 2.7"):
+            found["2.7"] = ENV_CONFIG["python_27_path"]
         else:
-            raise EnvironmentError('Python 2.7 path specified, but is not actually a Python 2.7 executable.')
+            raise EnvironmentError(
+                "Python 2.7 path specified, but is not actually a Python 2.7 executable."
+            )
 
-    if ENV_CONFIG['python_36_path']:
-        out, cmd = _try_run_python(ENV_CONFIG['python_36_path'])
-        if out.startswith('Python 3.6'):
-            found['3.6'] = ENV_CONFIG['python_36_path']
+    if ENV_CONFIG["python_36_path"]:
+        out, cmd = _try_run_python(ENV_CONFIG["python_36_path"])
+        if out.startswith("Python 3.6"):
+            found["3.6"] = ENV_CONFIG["python_36_path"]
         else:
-            raise EnvironmentError('Python 3.6 path specified, but is not actually a Python 3.6 executable.')
+            raise EnvironmentError(
+                "Python 3.6 path specified, but is not actually a Python 3.6 executable."
+            )
 
-    if not found['2.7'] or not found['3.6']:
+    if not found["2.7"] or not found["3.6"]:
         # try plain `python` first
-        out, cmd = _try_run_python('python')
-        if not found['2.7'] and out and out.startswith('Python 2.7'):
-            found['2.7'] = cmd
-        elif not found['3.6'] and out and out.startswith('Python 3.6'):
-            found['3.6'] = cmd
+        out, cmd = _try_run_python("python")
+        if not found["2.7"] and out and out.startswith("Python 2.7"):
+            found["2.7"] = cmd
+        elif not found["3.6"] and out and out.startswith("Python 3.6"):
+            found["3.6"] = cmd
 
-    if not found['2.7'] and out and (found['2.7'] is None):
-        out, cmd = _try_run_python('python2')
-        if out.startswith('Python 2.7'):
-            found['2.7'] = cmd
+    if not found["2.7"] and out and (found["2.7"] is None):
+        out, cmd = _try_run_python("python2")
+        if out.startswith("Python 2.7"):
+            found["2.7"] = cmd
 
-    if not found['3.6'] and out and found['3.6'] is None:
-        out, cmd = _try_run_python('python3')
-        if out.startswith('Python 3.6'):
-            found['3.6'] = cmd
+    if not found["3.6"] and out and found["3.6"] is None:
+        out, cmd = _try_run_python("python3")
+        if out.startswith("Python 3.6"):
+            found["3.6"] = cmd
 
     return found
